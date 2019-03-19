98fd0bda3dc15b83555f20de22cfe5c5a0435768
yuf <duanyufeii@qq.com>
2019-03-19 23:01:23 +0800

    上传

--- src/test/java/com/yuf/demo/sys/controller/GirlControllerTest.java
+++ src/test/java/com/yuf/demo/sys/controller/GirlControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class GirlControllerTest {
 	
 	@Autowired
