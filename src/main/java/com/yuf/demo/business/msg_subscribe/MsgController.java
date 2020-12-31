41d7b5115c3858e8ac4e286318de84fd8936c4e6
yuf <duanyufeii@qq.com>
2020-12-31 15:09:11 +0800

    新增冠林订阅上行接口

---
+++ src/main/java/com/yuf/demo/business/msg_subscribe/MsgController.java
@@ -0,0 +1,27 @@
+package com.yuf.demo.business.msg_subscribe;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: dyf
+ * @Date: 2020/12/31 10:21
+ */
+@Slf4j
+@RestController
+public class MsgController {
+
+
+    @PostMapping("/msg")
+    public void receiveMsg(@RequestBody GuanlinDoorInOutMsgDTO msgDTO){
+        log.info(JSONObject.toJSONString(msgDTO));
+    }
+
+}
---
+++ src/test/java/com/yuf/demo/business/msg_subscribe/MsgControllerTest.java
@@ -0,0 +1,16 @@
+package com.yuf.demo.business.msg_subscribe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MsgControllerTest {
+
+    @Test
+    public void receiveMsg() {
+
+
+
+
+    }
+}
