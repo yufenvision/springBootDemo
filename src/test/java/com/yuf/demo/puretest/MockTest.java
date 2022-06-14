1ef44f94184fb901db14cad132d123f6ecb9762f
duanyf4 <duanyf4@lenovo.com>
2022-06-14 13:46:24 +0800

    update mybatis generator plugins

--- src/test/java/com/yuf/demo/puretest/MockTest.java
+++ src/test/java/com/yuf/demo/puretest/MockTest.java
@@ -1,14 +1,9 @@
 package com.yuf.demo.puretest;
 
-import com.alibaba.fastjson.JSONObject;
-import com.yuf.demo.business.async_test.PersonInfo;
-import com.yuf.demo.business.async_test.TestController;
-import com.yuf.demo.utils.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -37,22 +32,4 @@ public class MockTest {
 		Assert.assertEquals(result, "list1");
 	}
 
-	@Test
-	public void test2(){
-		PersonInfo personInfo = mock(PersonInfo.class);
-		TestController testController = mock(TestController.class);
-
-		when(personInfo.getName()).thenReturn("名字");
-		when(personInfo.getId()).thenReturn("123");
-		when(personInfo.getAddress()).thenReturn("地址");
-		when(personInfo.getAge()).thenCallRealMethod();
-
-		when(testController.getPersonInfo("1")).thenReturn(new Response<PersonInfo>().success(personInfo));
-
-		PersonInfo p = (PersonInfo) testController.getPersonInfo("1").getData();
-		Assert.assertEquals(p.getAge(), null);
-		Assert.assertEquals(p.getName(), "名字");
-		Assert.assertEquals(p.getAddress(), "地址");
-		System.out.println(JSONObject.toJSONString(personInfo));
-	}
 }
---
+++ src/test/java/generator/MybatisGenerator.java
@@ -0,0 +1,20 @@
+package generator;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: dyf
+ * @Date: 2022/6/13 22:38
+ */
+public class MybatisGenerator {
+    /**
+     *  读取MBG生成代码
+     */
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
